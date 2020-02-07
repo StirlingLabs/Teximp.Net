cd0b9fda357069af4a0f4b5c22aed5a803b15af9
Starnick <nicholas.woodfield@gmail.com>
2020-02-07 18:08:52 -0500

    Updated change notes and version #s for 1.4.2 release

--- TeximpNet/Properties/AssemblyInfo.cs
+++ TeximpNet/Properties/AssemblyInfo.cs
@@ -31,7 +31,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Nicholas Woodfield")]
 [assembly: AssemblyProduct("TeximpNet")]
-[assembly: AssemblyCopyright("Copyright © 2016-2019 Nicholas Woodfield")]
+[assembly: AssemblyCopyright("Copyright © 2016-2020 Nicholas Woodfield")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -53,6 +53,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.4.1")]
-[assembly: AssemblyFileVersion("1.4.1")]
-[assembly: AssemblyInformationalVersion("1.4.1")]
+[assembly: AssemblyVersion("1.4.2")]
+[assembly: AssemblyFileVersion("1.4.2")]
+[assembly: AssemblyInformationalVersion("1.4.2")]
